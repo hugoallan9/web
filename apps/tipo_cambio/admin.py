f30db45577a8538d98958c5401d5fabb15e2e7d9
Hugo Garcia <hugoallangm@gmail.com>
2016-08-03 05:31:40 -0600

    Adiciones con Rabbitqm

--- apps/tipo_cambio/admin.py
+++ apps/tipo_cambio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Tipo_Cambio
+
 # Register your models here.
+admin.site.register(Tipo_Cambio)
--- apps/tipo_cambio/models.py
+++ apps/tipo_cambio/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Tipo_Cambio(models.Model):
+    fecha = models.DateTimeField('fecha de cambio')
+    cambio = models.FloatField()
